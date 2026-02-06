ab34b74b312e13ea4abcb616b7d1ac28c98c9afe
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-02-06 21:15:06 +0200

    add navbar/ showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import NavBar from "./components/Navbar";
 import Hero from "./sections/Hero";
+import ShowcaseSection from "./sections/ShowcaseSection";
 
 const App = () => {
   return (
     <>
+      <NavBar />
       <Hero />
+      <ShowcaseSection />
     </>
   )
 }
